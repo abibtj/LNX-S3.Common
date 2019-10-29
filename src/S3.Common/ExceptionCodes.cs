9006bb2d2ed8b182a69e5e219649057a5fb5c57f
abibtj <abibcode@gmail.com>
2019-10-29 17:55:38 +0100

    Added_Scores_Entry_Feature

--- src/S3.Common/ExceptionCodes.cs
+++ src/S3.Common/ExceptionCodes.cs
@@ -9,9 +9,11 @@ namespace S3.Common
         public static string InvalidEmail => "invalid_email";
         public static string InvalidPassword => "invalid_password";
         public static string InvalidRole => "invalid_role";
+        public static string ItemExists => "item_aready_exists";
         public static string RefreshTokenNotFound => "refresh_token_not_found";
         public static string RefreshTokenAlreadyRevoked => "refresh_token_already_revoked";
         public static string SchoolNameInUse => "school_name_in_use";
+        public static string SchoolAdminExists => "school_admin_already_exists";
         public static string NotFound => "not_found";
     }
 }
