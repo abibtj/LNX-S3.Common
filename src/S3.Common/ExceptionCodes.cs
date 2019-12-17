54de22fede2316089644d5c363489f2c4588c875
abibtj <abibcode@gmail.com>
2019-12-17 15:46:49 +0100

    Added_Notification_Service

--- src/S3.Common/ExceptionCodes.cs
+++ src/S3.Common/ExceptionCodes.cs
@@ -3,7 +3,6 @@ namespace S3.Common
     public static class ExceptionCodes
     {
         public static string Unauthorized => "unauthorized_action";
-        public static string UsernameInUse => "username_in_use";
         public static string InvalidCredentials => "invalid_credentials";
         public static string InvalidCurrentPassword => "invalid_current_password";
         public static string InvalidEmail => "invalid_email";
@@ -12,7 +11,7 @@ namespace S3.Common
         public static string ItemExists => "item_aready_exists";
         public static string RefreshTokenNotFound => "refresh_token_not_found";
         public static string RefreshTokenAlreadyRevoked => "refresh_token_already_revoked";
-        public static string SchoolNameInUse => "school_name_in_use";
+        public static string NameInUse => "name_in_use";
         public static string SchoolAdminExists => "school_admin_already_exists";
         public static string NotFound => "not_found";
     }
